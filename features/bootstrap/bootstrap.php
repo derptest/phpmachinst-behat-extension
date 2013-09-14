f55b81e875bf5b60a2a9e85947c630ddfd40aefb
Adam Englander <adam.englander@coupla.co>
2013-09-14 22:28:05 +0000

    Behat bootstrap fix Ony drop MySQL database if it exists

--- features/bootstrap/bootstrap.php
+++ features/bootstrap/bootstrap.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 // Setup Mysql Server For Test
 $pdo = new PDO('mysql:host=localhost;port=3307', 'root', null, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-$pdo->exec("DROP DATABASE testdb");
+$pdo->exec("DROP DATABASE IF EXISTS testdb");
 $pdo->exec("CREATE DATABASE testdb");
 $pdo->exec("USE testdb");
 $pdo->exec("CREATE TABLE user (id INTEGER PRIMARY KEY AUTO_INCREMENT, username VARCHAR(64), companyId INTEGER, active BIT)");
